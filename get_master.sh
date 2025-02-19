b4ac7cc16e0075a82f43b166ab197d7122affbfc
Christoph Junghans <junghans@votca.org>
2025-02-19 15:12:59 -0700

    CI: switch to mock

--- get_master.sh
+++ get_master.sh
@@ -1,1 +1,1 @@
-wget -O ArborX-1.7.tar.gz https://github.com/ArborX/ArborX/archive/refs/heads/master.tar.gz
+curl -o ArborX-1.7.tar.gz -L https://github.com/ArborX/ArborX/archive/refs/heads/master.tar.gz
